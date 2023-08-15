0fb326fccfcdb2b6f48b252397715701b5923ed2
b21ricni <b21ricni@student.his.se>
2023-08-15 02:46:28 +0200

    Created base
    Added: Name, Race and Personality
    Made function to create them
    TODO: Give unique ID to key props, add local storage, add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+  const [character, setCharacter] = useState([])
+  const [newCharacterName, setNewCharacterName] = useState("")
+  const [newCharacterRace, setNewCharacterRace] = useState("")
+  const [newCharacterPersonality, setNewCharacterPersonality] = useState("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setCharacter((currentCharacter) => {
+      return [
+        ...currentCharacter,
+        { id: crypto.randomUUID(), name: newCharacterName, race: newCharacterRace, personality: newCharacterPersonality, marked: false },
+      ]
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <>
+    <form onSubmit={handleSubmit} className="new-character-form">
+      <div className="form-row">
+        <label htmlFor="">Name</label>
+        <input 
+          value={newCharacterName} 
+          onChange={e => setNewCharacterName(e.target.value)} 
+          type="text" 
+          id="name-input" 
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="form-row">
+        <label htmlFor="">Race</label>
+        <input 
+          value={newCharacterRace} 
+          onChange={e => setNewCharacterRace(e.target.value)}
+          type="text" 
+          id="race-input" 
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="form-row">
+        <label htmlFor="">Personality</label>
+        <input 
+          value={newCharacterPersonality} 
+          onChange={e => setNewCharacterPersonality(e.target.value)} 
+          type="text" 
+          id="personality-input" 
+        />
+      </div>
+
+      <button className="btn btn-create-character">Create Character</button>
+    </form>
+
+    <h1 className="header">Characters</h1>
+    <ul className="list">
+      {character.map(character => {
+        return (
+          <li>
+            <label>
+              <input type="checkbox" checked={character.marked}/>
+              {character.name}
+              {character.race}
+              {character.personality}
+            </label>
+            <button className="btn btn-delete-character">X</button>
+          </li>
+        )
+      })}
+    </ul>
+  </>
   )
 }
-
-export default App
